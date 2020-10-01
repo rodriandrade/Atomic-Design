6df261b61f12577b655bddfcb2ff71f66e253ac4
rodriandrade <rodriandrade1498@gmail.com>
2020-10-01 10:23:37 -0300

    Auth0

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,7 +2,14 @@ import React from 'react'
 import {List, Ul} from './styled'
 import { Link, withRouter } from "react-router-dom";
 
+import { useAuth0 } from "@auth0/auth0-react";
+import LogOutButton from "../LogOutButton"
+import LoginButton from "../LoginButton";
+
 const Nav = () =>{
+
+    const { isAuthenticated } = useAuth0();
+
     return(
     <nav>
         <Ul>
@@ -11,6 +18,8 @@ const Nav = () =>{
             <List><Link to="/about">About</Link></List>
             <List><Link to="/profile">Profile</Link></List>
         </Ul>
+
+        {isAuthenticated ? <LogOutButton /> : <LoginButton />}
     </nav>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/index';
+import { BrowserRouter as Router } from "react-router-dom";
+import Auth0ProviderWithHistory from "./components/auth0-provider-with-history";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
+  </Router>,
+  document.getElementById("root")
 );
-

4ebec71d0324e40e853b793f881ee028c3ae8440
Nayla <nayla.mar2@gmail.com>
2020-10-03 03:25:28 -0300

    menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Inner, Nav, Container, ParticlesBackground } from '../components/Index'
+
+const Menu = () => {
+    return (
+        <Inner>
+            <Container direction="column">
+                <Nav />
+            </Container>
+        </Inner>
+    )
+}
+
+export default Menu
